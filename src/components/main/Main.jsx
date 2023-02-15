5f2918a96c292ac4e22dc9cec11d0017b9970e69
Dmitriy <tiemposilencioo@gmail.com>
2023-02-15 19:55:22 +0300

    sketches

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,39 +1,45 @@
+import cat from 'src/assets/img/cat.svg';
 import './main.scss';
 
 const Main = () => {
     return (
         <section className="main">
-            <h1 className="main__title">Ты сегодня покормил кота?</h1>
-            <div className="main__item">
-                <div className="main__item-title">
-                    <p>Сказочное заморское яство</p>
-                    <h2>Нямушка</h2>
-                    <p>с фуа-гра</p>
-                    <p>10 порций</p>
-                    <p>мышь в подарок</p>
+            <div className="main__wrapper">
+                <h1 className="main__title">Ты сегодня покормил кота?</h1>
+                <div className="main__item">
+                    <div className="main__item-title">
+                        <p>Сказочное заморское яство</p>
+                        <h2>Нямушка</h2>
+                        <p>с фуа-гра</p>
+                        <p>10 порций</p>
+                        <p>мышь в подарок</p>
+                    </div>
+                    <img src={cat} alt="" className="main__cat" />
+                    <div className="main__round"></div>
                 </div>
-                <img src="src/assets/cat.webP" alt="" className="cat" />
-            </div>
-            <div className="main__item">
-                <div className="main__item-title">
-                    <p>Сказочное заморское яство</p>
-                    <h2>Нямушка</h2>
-                    <p>с рыбой</p>
-                    <p>40 порций</p>
-                    <p>2 мыши в подарок</p>
+                <div className="main__item">
+                    <div className="main__item-title">
+                        <p>Сказочное заморское яство</p>
+                        <h2>Нямушка</h2>
+                        <p>с рыбой</p>
+                        <p>40 порций</p>
+                        <p>2 мыши в подарок</p>
+                    </div>
+                    <img src={cat} alt="" className="main__cat" />
+                    <div className="main__round"></div>
                 </div>
-                <img src="" alt="" className="cat" />
-            </div>
-            <div className="main__item">
-                <div className="main__item-title">
-                    <p>Сказочное заморское яство</p>
-                    <h2>Нямушка</h2>
-                    <p>с курой</p>
-                    <p>100 порций</p>
-                    <p>5 мышей в подарок</p>
-                    <p>Заказчик доволен</p>
+                <div className="main__item">
+                    <div className="main__item-title">
+                        <p>Сказочное заморское яство</p>
+                        <h2>Нямушка</h2>
+                        <p>с курой</p>
+                        <p>100 порций</p>
+                        <p>5 мышей в подарок</p>
+                        <p>Заказчик доволен</p>
+                    </div>
+                    <img src={cat} alt="" className="main__cat" />
+                    <div className="main__round"></div>
                 </div>
-                <img src="" alt="" className="cat" />
             </div>
         </section>
     )
