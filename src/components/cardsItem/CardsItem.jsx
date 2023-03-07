540f61ff052595e9857526bb2ca204e82ad70c39
Dmitriy <tiemposilencioo@gmail.com>
2023-03-07 11:23:09 +0300

    complit

--- src/components/cardsItem/CardsItem.jsx
+++ src/components/cardsItem/CardsItem.jsx
@@ -17,51 +17,51 @@ const CardsItem = ({ id, title, subtitle, hoverText, activeTaste, buyDescr, disB
             <div className="cardsItem__wrapp">
                 <div className="cardsItem__wrappCorner" onMouseLeave={() => activeHover(true)}>
                     <div className={
-                        active ? "activeCorner cardsItem__corner" : "cardsItem__corner"
+                        active ? "cardsItem__corner_active cardsItem__corner" : "cardsItem__corner"
                             &&
-                            disabledCard ? "disabledCorner cardsItem__corner" : "cardsItem__corner"
+                            disabledCard ? "cardsItem__corner_disabled cardsItem__corner" : "cardsItem__corner"
                     } onClick={(e) => activeTogle(e, true)}></div>
                     <div className={
-                        active ? "activeRectangle cardsItem__rectangle" : "cardsItem__rectangle"
+                        active ? "cardsItem__rectangle_active cardsItem__rectangle" : "cardsItem__rectangle"
                             &&
-                            disabledCard ? "disabledRectangle cardsItem__rectangle" : "cardsItem__rectangle"
+                            disabledCard ? "cardsItem__rectangle_disabled cardsItem__rectangle" : "cardsItem__rectangle"
                     } onClick={(e) => activeTogle(e, true)}> <h2 className={
-                        hover ? "activeHoverText cardsItem__subtitle" : "cardsItem__subtitle"
+                        hover ? "cardsItem__subtitle_hover cardsItem__subtitle" : "cardsItem__subtitle"
                             &&
-                            disabledCard ? "disabledText cardsItem__subtitle" : "cardsItem__subtitle"
+                            disabledCard ? "cardsItem__subtitle_disabled cardsItem__subtitle" : "cardsItem__subtitle"
                     } >{hover ? hoverText : subtitle}</h2></div>
                 </div>
                 <div className="cardsItem__wrappCards" onMouseLeave={() => activeHover(true)}>
                     <div className={
-                        active ? "activeCard cardsItem__card" : "cardsItem__card"
+                        active ? "cardsItem__card_active cardsItem__card" : "cardsItem__card"
                             &&
-                            disabledCard ? "disabledWrapp cardsItem__card" : "cardsItem__card"
+                            disabledCard ? "cardsItem__card_disabled cardsItem__card" : "cardsItem__card"
                     } onClick={(e) => activeTogle(e, true)}>
-                        <h3 className={disabledCard ? "disabledText cardsItem__title" : "cardsItem__title"}>{title}</h3>
-                        <p className={disabledCard ? "disabledText cardsItem__taste" : "cardsItem__taste"}>{taste}</p>
+                        <h3 className={disabledCard ? "cardsItem__title_disabled cardsItem__title" : "cardsItem__title"}>{title}</h3>
+                        <p className={disabledCard ? "cardsItem__taste_disabled cardsItem__taste" : "cardsItem__taste"}>{taste}</p>
                         <div className="cardsItem__portionsWrapp">
-                            <p className={disabledCard ? "disabledText cardsItem__portions" : "cardsItem__portions"}>{portions}</p> &nbsp;
-                            <p className={disabledCard ? "disabledText cardsItem__portionsDescr" : "cardsItem__portionsDescr"}>{portionsDescr}</p>
+                            <p className={disabledCard ? "cardsItem__portions_disabled cardsItem__portions" : "cardsItem__portions"}>{portions}</p> &nbsp;
+                            <p className={disabledCard ? "cardsItem__portionsDescr_disabled cardsItem__portionsDescr" : "cardsItem__portionsDescr"}>{portionsDescr}</p>
                         </div>
-                        <div className={disabledCard ? "disabledText cardsItem__giftWrapp" : "cardsItem__giftWrapp"}>
+                        <div className={disabledCard ? "cardsItem__giftWrapp_disabled cardsItem__giftWrapp" : "cardsItem__giftWrapp"}>
                             <div className="cardsItem__gift"> {gift === '' ? null : gift}
                                 {gift === '' ? null : `\u00A0`}
                             </div>
                             <p className="cardsItem__giftDescr">{giftDescr}</p>
                         </div>
-                        <p className={disabledCard ? "disabledText cardsItem__giftDescrOptions" : "cardsItem__giftDescrOptions"}>{giftDescrOptions} </p>
-                        <img src={cat} alt="cat" className={disabledCard ? "disabledImg cardsItem__img" : "cardsItem__img"} />
+                        <p className={disabledCard ? "cardsItem__giftDescrOptions_disabled cardsItem__giftDescrOptions" : "cardsItem__giftDescrOptions"}>{giftDescrOptions} </p>
+                        <img src={cat} alt="cat" className={disabledCard ? "cardsItem__img_disabled cardsItem__img" : "cardsItem__img"} />
                         <div className={
-                            active ? "activeRound  cardsItem__round" : "cardsItem__round"
+                            active ? "cardsItem__round_active  cardsItem__round" : "cardsItem__round"
                                 &&
-                                disabledCard ? "disabledRound cardsItem__round" : "cardsItem__round"}>
+                                disabledCard ? "cardsItem__round_disabled cardsItem__round" : "cardsItem__round"}>
                             <div className="cardsItem__weight">{weight}</div>
                             <div className="cardsItem__kg">кг</div>
                         </div>
                     </div>
                 </div>
                 <div className="cardsItem__wrappBuyDescr">
-                    <p className={disabledCard ? "disabledBuyDescr cardsItem__buyDescr" : "cardsItem__buyDescr"}>{
+                    <p className={disabledCard ? "cardsItem__buyDescr_disabled cardsItem__buyDescr_disabled" : "cardsItem__buyDescr"}>{
                         active ? activeTaste : buyDescr
                             &&
                             disabledCard ? disBuyDescr : buyDescr
